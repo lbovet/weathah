6e4b2e9677c8c33d5423b3f87a868c46d095d5e7
Laurent Bovet <laurent.bovet@windmaster.ch>
2021-05-16 13:16:50 +0200

    Implement

--- index.js
+++ index.js
@@ -3,26 +3,60 @@ const express = require('express')
 const got = require('got')
 const moment = require('moment')
 const config = require('./config')
+const fs = require('fs')
 
 const app = express()
 const port = 3001
 
 var cached = {}
 
-app.get('/forecasts', (req, res) => {
-    got.post(config.authUrl, {
-        responseType: 'json',
-        headers: {
-            Authorization: "Basic " + config.credentials
+fs.readFile('forecasts.json', (err, data) => {
+    cached = JSON.parse(data)
+})
+
+function shouldGet() {
+    if(cached && cached.fetched) {
+        if(moment(cached.fetched).isBefore(moment().subtract(4, 'hours'))) {
+            return true
         }
-    })
-    .then(result => {
-        console.log('token: '+result.data)
-    })
-    .then(result => {
-        res.set('Content-Type', 'application/json')
+    } else {
+        return true
+    }
+    return false
+}
+
+app.get('/forecasts', (req, res) => {
+    res.set('Content-Type', 'application/json')
+    if(shouldGet()) {
+        got.post(config.authUrl, {
+            responseType: 'json',
+            headers: {
+                Authorization: "Basic " + config.credentials
+            }
+        }).then(result => {
+            console.log('token: ' + result.body.access_token)
+            return got.get(config.forecastUrl, {
+                responseType: 'json',
+                headers: {
+                    Authorization: "Bearer " + result.body.access_token
+                }
+            })
+        }).then(result => {
+            cached = {
+                fetched: moment().format(),
+                available: result.headers['x-ratelimit-available'],
+                forecasts: result.body
+            }
+            fs.writeFile('forecasts.json', JSON.stringify(cached), ()=>{})
+            res.send(JSON.stringify(cached))
+        }, err => {
+            console.log(err.message)
+            res.send(JSON.stringify(cached))
+        })
+    } else {
+        console.log('returning cached data')
         res.send(JSON.stringify(cached))
-    })
+    }
 })
 
 app.listen(port, () => {
